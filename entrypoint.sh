e2b47413a389ae466fadd60a278c481bfc68aa3d
Shawn Wang <shawn.wang@ericsson.com>
2021-12-16 17:40:05 +0800

    Use supervisord to work as entrypoint
    Change-Id: I0e49c9b01f5e4fa0fc0f9ca6a74c86cacf129fdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-if [ -z "$SecurityTypes" ]; then
-    SecurityTypes="VncAuth,TLSVnc"
-fi
-if [ -z "$geometry" ]; then
-    geometry="1920x1080"
-fi
-if [ -z "$depth" ]; then
-    depth=24
+
+if [[ ! -d /run/sshd ]]; then
+    mkdir -p /run/sshd
+    echo root:123456 | chpasswd
 fi
 
-/usr/bin/tigervncserver -rfbport 5901 \
-    -localhost=0 -SecurityTypes $SecurityTypes \
-    -PasswordFile /root/.vnc/passwd \
-    -ClientWaitTimeMillis 30000 \
-    -geometry "$geometry" \
-    -depth "$depth" \
-    -useold
-sleep infinity
+if ! grep "^PermitRootLogin yes" /etc/ssh/sshd_config > /dev/null; then
+    echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+fi
+if ! grep "^PasswordAuthentication yes" /etc/ssh/sshd_config > /dev/null; then
+    echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+fi
+
+exec supervisord -n -c /etc/supervisor/supervisord.conf
